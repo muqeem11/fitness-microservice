6048b7bc878f57dd735c199cc69cef1a68f80716
Mohammad muqeem <Muqeemmekrani9@gmail.com>
2026-02-06 00:06:02 +0530

    Add gateway and configserver microservices

---
+++ configserver/src/test/java/com/fitness/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fitness.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootTest
+@Configuration
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
